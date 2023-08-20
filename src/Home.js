8ed061bc863aceadcc003794f4485b8decca185f
azizkhadraoui <142397889+azizkhadraoui@users.noreply.github.com>
2023-08-20 13:21:19 +0100

    changes

--- src/Home.js
+++ src/Home.js
@@ -1,13 +1,31 @@
 import React from "react";
 import app from "./base";
-import { getAuth,signOut} from "firebase/auth";
+import { getAuth, signOut } from "firebase/auth";
+import { getFirestore, updateDoc, doc } from "firebase/firestore"; // Import Firestore methods
+
 const auth = getAuth(app);
 
 const Home = () => {
+  const handleSignOut = async () => {
+    try {
+      await updateUserStatus("logged-out");
+      await signOut(auth);
+
+    } catch (error) {
+      console.error("Error signing out:", error);
+    }
+  };
+
+  const updateUserStatus = async (status) => {
+    const firestore = getFirestore(app);
+    const userDocRef = doc(firestore, "users", auth.currentUser.uid);
+    await updateDoc(userDocRef, { userstatus: status });
+  };
+
   return (
     <>
       <h1>Home</h1>
-      <button onClick={() => signOut(auth)}>Sign out</button>
+      <button onClick={handleSignOut}>Sign out</button>
     </>
   );
 };
--- src/l.js
+++ src/l.js
@@ -1,42 +1,94 @@
-import React, { useCallback, useContext } from "react";
+import React, { useCallback, useContext, useState, useEffect } from "react";
 import { withRouter, Redirect } from "react-router";
 import app from "./base.js";
 import { AuthContext } from "./Auth.js";
-import { getAuth,signInWithEmailAndPassword, signInWithPopup, GoogleAuthProvider, FacebookAuthProvider } from "firebase/auth";
+import {signOut, getAuth, signInWithEmailAndPassword, signInWithPopup, GoogleAuthProvider, FacebookAuthProvider } from "firebase/auth";
+import { getFirestore, updateDoc, doc, getDoc } from "firebase/firestore";
+
 const auth = getAuth(app);
 const googleProvider = new GoogleAuthProvider();
 const facebookProvider = new FacebookAuthProvider();
 
-
 const Login = ({ history }) => {
+  const [userStatus, setUserStatus] = useState("");
+
+  const fetchUserStatus = async () => {
+    const firestore = getFirestore(app);
+    const userDocRef = doc(firestore, "users", auth.currentUser.uid);
+    const userDoc = await getDoc(userDocRef);
+
+    if (userDoc.exists() && userDoc.data() && userDoc.data().userstatus) {
+      setUserStatus(userDoc.data().userstatus);
+      return(userDoc.data().userstatus)
+      
+
+    } else {
+      console.log("User status data is missing.");
+    }
+  };
+
   const handleLogin = useCallback(
     async event => {
       event.preventDefault();
       const { email, password } = event.target.elements;
+
       try {
-        await signInWithEmailAndPassword(auth,email.value, password.value);
+        await signInWithEmailAndPassword(auth, email.value, password.value);
+        const status = await fetchUserStatus(); // Await here
+        if(status == "logged-in"){
+          alert("already logged in");
+          signOut(auth);
+        }
+        else{
+          await updateUserStatus("logged-in");
         history.push("/");
+        }
+        
       } catch (error) {
         alert(error.message);
       }
     },
     [history]
   );
+
   const handleGoogleLogin = async () => {
     try {
       await signInWithPopup(auth, googleProvider);
-      history.push("/");
+      const status = await fetchUserStatus(); // Await here
+        if(status == "logged-in"){
+          alert("already logged in");
+          signOut(auth);
+        }
+        else{
+          await updateUserStatus("logged-in");
+        history.push("/");
+        }
     } catch (error) {
       alert(error.message);
     }
   };
+
   const handleFacebookLogin = async () => {
     try {
       await signInWithPopup(auth, facebookProvider);
-      history.push("/");
+      const status = await fetchUserStatus(); // Await here
+        if(status == "logged-in"){
+          alert("already logged in");
+          signOut(auth);
+        }
+        else{
+          await updateUserStatus("logged-in");
+        history.push("/");
+        }
     } catch (error) {
       alert(error.message);
     }
+  };
+
+  const updateUserStatus = async (status) => {
+    const firestore = getFirestore(app);
+    const userDocRef = doc(firestore, "users", auth.currentUser.uid);
+    await updateDoc(userDocRef, { userstatus: status });
   };
 
   const { currentUser } = useContext(AuthContext);
