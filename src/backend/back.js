96fe1c64139091bb1180d39cbc8122e79547eaa4
azizkhadraoui <142397889+azizkhadraoui@users.noreply.github.com>
2023-09-07 20:17:10 +0100

    backend

---
+++ src/backend/back.js
@@ -0,0 +1,41 @@
+import express from "express"
+import mysql from "mysql"
+import cors from "cors"
+
+const app = express();
+const db = mysql.createConnection({
+    host:"localhost",
+    user:"root",
+    password:"aziz989120011012003",
+    database:"air_exam"
+})
+
+app.use(express.json())
+app.use(cors());
+app.get("/",(req,res) => {
+    res.json("this is the backend side")
+})
+app.get("/data",(req,res) => {
+    const q = " SELECT * FROM air_exam.questions LIMIT 10; "
+    db.query(q,(err,data)=>{
+        if(err) return res.json(err)
+        return res.json(data)
+    })
+
+})
+app.post("/books", (req, res) => {
+    const q = "INSERT INTO books(`title`, `desc`, `cover`) VALUES (?)";
+  
+    const values = [
+      req.body.title,
+      req.body.desc,
+      req.body.cover,
+    ];
+  
+    db.query(q, [values], (err, data) => {
+      if (err) return res.send(err);
+      return res.json(data);
+    });
+  });
+app.listen(8800, () =>{console.log("connected to backend")
+});
