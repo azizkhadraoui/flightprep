1af6f2fa78dc8af520e969155feb59e048594d6b
FediMechergui <fedimechergui03@gmail.com>
2023-10-05 14:43:14 +0100

    message

--- src/base.js
+++ src/base.js
@@ -3,13 +3,13 @@ import "firebase/auth";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCZzzFLz-foMPHChXvDLqJY7WCm6979DzE",
-  authDomain: "flightprep-ece47.firebaseapp.com",
-  projectId: "flightprep-ece47",
-  storageBucket: "flightprep-ece47.appspot.com",
-  messagingSenderId: "462278988225",
-  appId: "1:462278988225:web:0945f91a38ee1526e69f40",
-  measurementId: "G-K91P1YTKCQ"
+  apiKey: process.env.REACT_APP_FIREBASE_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
