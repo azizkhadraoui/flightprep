6e6886558f042bcfb3f9858a6085fe5a7df36998
FediMechergui <fedimechergui03@gmail.com>
2023-11-30 16:14:39 +0100

    message

--- src/components/questionelements/ExamQuestion.jsx
+++ src/components/questionelements/ExamQuestion.jsx
@@ -1,14 +1,20 @@
-import React, { useState, useEffect } from 'react';
-import { Typography, Card, CardContent } from '@mui/material';
-import app from '../../base.js';
-import { getFirestore, doc, setDoc, getDoc } from 'firebase/firestore';
-import { getAuth, onAuthStateChanged } from 'firebase/auth';
-import { useHistory } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { Typography, Card, CardContent } from "@mui/material";
+import app from "../../base.js";
+import { getFirestore, doc, setDoc, getDoc } from "firebase/firestore";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { useHistory } from "react-router-dom";
 
 const db = getFirestore(app);
 const auth = getAuth(app);
 
-const ExamQuestion = ({ questions, currentQuestion, setCurrentQuestion, selectedAnswers, setSelectedAnswers }) => {
+const ExamQuestion = ({
+  questions,
+  currentQuestion,
+  setCurrentQuestion,
+  selectedAnswers,
+  setSelectedAnswers,
+}) => {
   const [selectedAnswer, setSelectedAnswer] = useState(null);
   const [currentUserId, setCurrentUserId] = useState(null);
   const history = useHistory();
@@ -27,17 +33,21 @@ const ExamQuestion = ({ questions, currentQuestion, setCurrentQuestion, selected
 
   const handleAnswerClick = async (answerKey) => {
     if (!currentUserId) {
-      console.error('User not authenticated');
+      console.error("User not authenticated");
       return;
     }
-  
-    const isCorrectAnswer = answerKey === questions[currentQuestion].correct.toUpperCase();
+
+    const isCorrectAnswer =
+      answerKey === questions[currentQuestion].correct.toUpperCase();
     const questionId = questions[currentQuestion].id;
-  
+
     try {
-      const userChoicesRef = doc(db, `users/${currentUserId}/user_choices/${questionId}`);
+      const userChoicesRef = doc(
+        db,
+        `users/${currentUserId}/user_choices/${questionId}`
+      );
       const userChoicesDoc = await getDoc(userChoicesRef);
-  
+
       if (!userChoicesDoc.exists()) {
         await setDoc(userChoicesRef, {
           questionId: questionId,
@@ -46,11 +56,11 @@ const ExamQuestion = ({ questions, currentQuestion, setCurrentQuestion, selected
         });
       }
     } catch (error) {
-      console.error('Error writing document: ', error);
+      console.error("Error writing document: ", error);
     }
-  
+
     // Update the selected answer for the current question
-    setSelectedAnswers(prevAnswers => {
+    setSelectedAnswers((prevAnswers) => {
       const newAnswers = [...prevAnswers];
       newAnswers[currentQuestion] = answerKey;
       return newAnswers;
@@ -61,21 +71,35 @@ const ExamQuestion = ({ questions, currentQuestion, setCurrentQuestion, selected
     setSelectedAnswer(null);
   }, [currentQuestion]);
 
-
   return (
     <div>
       {questions.length > 0 ? (
         <div>
-          <Typography variant="h6" color="white">{questions[currentQuestion]?.question}</Typography>
-          <div onClick={() => history.push(`/canvas/${questions[currentQuestion]?.imageUrl}`)}>
-            <img src={questions[currentQuestion]?.imageUrl} alt="question related" style={{width: '100px', height: '100px'}}/>
+          <Typography variant="h6" color="white">
+            {questions[currentQuestion]?.question}
+          </Typography>
+          <div
+            onClick={() =>
+              history.push(`/canvas/${questions[currentQuestion]?.imageUrl}`)
+            }
+          >
+            <img
+              src={questions[currentQuestion]?.imageUrl}
+              alt="question related"
+              style={{ width: "100px", height: "100px" }}
+            />
           </div>
-          {['A', 'B', 'C', 'D'].map((key) => (
-            <div
-              key={key}
-            >
-              <Card style={selectedAnswers[currentQuestion] === key ? { backgroundColor: '#FFA500', color: '#FFFFFF' } : null}
-              onClick={() => handleAnswerClick(key)}>
+          {["A", "B", "C", "D"].map((key) => (
+            <div key={key}>
+              <Card
+                sx={{ marginBottom: "10px" }}
+                style={
+                  selectedAnswers[currentQuestion] === key
+                    ? { backgroundColor: "#FFA500", color: "#FFFFFF" }
+                    : { backgroundColor: "#FFFFFF", color: "#000000" }
+                }
+                onClick={() => handleAnswerClick(key)}
+              >
                 <CardContent>
                   <Typography variant="body1">
                     {key}: {questions[currentQuestion][key]}
@@ -86,7 +110,9 @@ const ExamQuestion = ({ questions, currentQuestion, setCurrentQuestion, selected
           ))}
         </div>
       ) : (
-        <Typography variant="h6" color="white">Loading...</Typography>
+        <Typography variant="h6" color="white">
+          Loading...
+        </Typography>
       )}
     </div>
   );
